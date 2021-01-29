962b1476feb16a353123086e159bd7e4dd71a1eb
lcorcodilos <lac352@scarletmail.rutgers.edu>
2021-01-29 16:45:40 -0500

    GroupImgToPdf: Add captions and columns options

--- GroupImgToPdf.py
+++ GroupImgToPdf.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 from pylatex import Document, Figure, NoEscape, MiniPage
+import math
 
-def GroupImgToPdf(plot_files,outname,margin="0.5in"):
+def GroupImgToPdf(plot_files,outname,ncols,captionBool,margin="0.5in"):
     # Write some LaTeX
     tex = Document(geometry_options={"margin":margin},page_numbers=False)
-    for iplot in range(0,len(plot_files),2):
-        plot_left = plot_files[iplot]
-        if iplot+1 < len(plot_files):
-            plot_right = plot_files[iplot+1]
-        else:
-            plot_right = False
 
+    indexing = GetIndexing(len(plot_files),ncols)
+
+    for irow in indexing:
+        these_plots = [plot_files[iplot] for iplot in irow]
         with tex.create(Figure(position='h!')) as fig:
-            fig.add_image(plot_left,width=NoEscape(r'0.45\textwidth'))
-            if plot_right != False: fig.add_image(plot_right,width=NoEscape(r'0.45\textwidth'))
+            for plot in these_plots:
+                fig.add_image(plot,width=NoEscape(r'%s\textwidth'%(0.97/len(these_plots))))
+                if captionBool:
+                    fig.add_caption(plot)
 
     tex.generate_pdf(outname,clean_tex=False)
+
+def GetIndexing(nplots,ncols):
+    indexing = []
+    nrows = math.ceil(nplots/ncols)
+    for irows in range(0,nrows):
+        if irows <= (nrows-1):
+            indexing.append([irows + i for i in range(ncols)])
+        else:
+            indexing.append([irows + i for i in range(nplots%ncols)])
+    return indexing
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -33,6 +45,16 @@ if __name__ == "__main__":
                     dest      =   'margin',
                     help      =   'Margin (with units)')
 
+    parser.add_argument('--columns', metavar='F', action='store', type=int,
+                    default   =   1,
+                    dest      =   'columns',
+                    help      =   'Number of columns per page')
+
+    parser.add_argument('--captions', action='store_true',
+                    default   =   False,
+                    dest      =   'captions',
+                    help      =   'Add captions with file path')
+
     parser.add_argument(dest = 'plotfiles', metavar='FILENAMES', type=str, nargs='+',
                     help='Plot file names')
 
@@ -45,4 +67,4 @@ if __name__ == "__main__":
         else:
             plots.append(f)
 
-    GroupImgToPdf(plots,options.output,margin=options.margin)
+    GroupImgToPdf(plots,options.output,options.columns,options.captions,margin=options.margin)
