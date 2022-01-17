3272c4331368f2d1edc815c96cc8ca9085dced6b
lcorcodilos <lac352@scarletmail.rutgers.edu>
2022-01-17 14:56:34 -0500

    Port some functions that were laying around

--- CorcLib/common.py
+++ CorcLib/common.py
@@ -30,3 +30,64 @@ def lib_path():
             os.path.abspath(__file__)
             )
         )
+
+def dict_to_tex_table(dict2convert,outfilename,roworder=[],columnorder=[]):
+    # First set of keys are row, second are column
+    if len(roworder) == 0:
+        rows = sorted(dict2convert.keys())
+    else:
+        rows = roworder
+    if len(columnorder) == 0:
+        columns = []
+        for r in rows:
+            thesecolumns = dict2convert[r].keys()
+            for c in thesecolumns:
+                if c not in columns:
+                    columns.append(c)
+        columns.sort()
+    else:
+        columns = columnorder
+
+    latexout = open(outfilename,'w')
+    latexout.write('\\begin{table}[] \n')
+    latexout.write('\\begin{tabular}{|c|'+len(columns)*'c'+'|} \n')
+    latexout.write('\\hline \n')
+
+    column_string = ' &'
+    for c in columns:
+        column_string += str(c)+'\t& '
+    column_string = column_string[:-2]+'\\\ \n'
+    latexout.write(column_string)
+
+    latexout.write('\\hline \n')
+    for r in rows:
+        row_string = '\t'+r+'\t& '
+        for c in columns:
+            if c in dict2convert[r].keys():
+                row_string += str(dict2convert[r][c])+'\t& '
+            else:
+                row_string += '- \t& '
+        row_string = row_string[:-2]+'\\\ \n'
+        latexout.write(row_string)
+
+    latexout.write('\\hline \n')
+    latexout.write('\\end{tabular} \n')
+    latexout.write('\\end{table}')
+    latexout.close()
+
+def find_common_string(string_list):
+    to_match = ''   # initialize the string we're looking for/building
+    for s in string_list[0]:    # for each character in the first string
+        passed = True
+        for istring in range(1,len(string_list)):   # compare to_match+s against strings in string_list
+            string = string_list[istring]
+            if to_match not in string:                  # if in the string, add more
+                passed = False
+            
+        if passed == True:
+            to_match+=s
+
+    if to_match[-2] == '_':
+        return to_match[:-2] 
+    else:
+        return to_match[:-1]                # if not, return to_match minus final character
